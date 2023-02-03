6b42b43d99d4dda047c58c747fa4de8002cbc3e5
nguyenhuudung201 <115616484+nguyenhuudung201@users.noreply.github.com>
2023-02-03 09:09:35 +0700

    sql
    sql

---
+++ brand.sql
@@ -0,0 +1,15 @@
+create table brands (
+id int primary key identity(1,1),
+name varchar(50) not null ,
+address varchar(255) not null ,
+phone_number varchar(255) not null unique,
+);
+create table product (
+id int primary key identity(1,1),
+name varchar(255) not null ,
+describe text not null ,
+unit varchar(50) not null,
+price int not null,
+amount int not null,
+brand_id int foreign key references brands(id),
+);
